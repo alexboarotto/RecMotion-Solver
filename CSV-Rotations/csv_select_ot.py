398d4e6e214ef7c4aae4a7fd2c16586650db1f78
Alexboa <alexboarotto@gmail.com>
2022-05-03 09:18:28 +0200

    extract rotations from data

--- CSV-Rotations/csv_select_ot.py
+++ CSV-Rotations/csv_select_ot.py
@@ -5,6 +5,39 @@ from bpy.props import StringProperty
 
 from . data import Data
 
+# Load and return json file
+def load_data(path):
+    file = open(path, mode='r')
+    data = file.readlines()
+    file.close()
+
+    roll_index = None
+    pitch_index = None
+    yaw_index = None
+
+    for index, elem in enumerate(data[0].split(",")):
+        if elem.strip() == "Roll":
+            roll_index = index
+        if elem.strip() == "Pitch":
+            pitch_index = index
+        if elem.strip() == "Yaw":
+            yaw_index = index
+    
+    data.pop(0)
+
+    rotations = []
+
+    for i in data:
+        rotation = []
+        list = i.split(",")
+        rotation.append(list[roll_index])
+        rotation.append(list[pitch_index])
+        rotation.append(list[yaw_index])
+        rotations.append(rotation)
+    
+
+    return rotations
+
 
 class ImportCSV_OT(Operator, ImportHelper):
     """Import CSV"""
@@ -14,13 +47,19 @@ class ImportCSV_OT(Operator, ImportHelper):
     filename_ext = '.txt'
     
     filter_glob: StringProperty(
-        default='*.txt;*.csv',
+        default='*.txt',
         options={'HIDDEN'}
     )
  
     def execute(self, context):
-        print('imported file: ', self.filepath)
-        Data.csv = 1
+        try:
+            Data.csv = load_data(self.filepath)
+            print(str(Data.csv))
+            error = None
+        except Exception as err:
+            error = err.args[0]
+        if error is not None:
+            self.report({'ERROR'}, error)
         return {'FINISHED'}
 
 def register():
--- CSV-Rotations/data.py
+++ CSV-Rotations/data.py
@@ -5,10 +5,5 @@ import bpy
 class Data:
     csv = None
 
-    # Setup global materials for each interaction zone type
-    @classmethod
-    def create_iz_materials(self):
-        pass
-
    
         
